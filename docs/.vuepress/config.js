64be8fc990448c293fd8a7f73acd7f5ff09e0e56
ergun.kilic <ergun.kilic@he-arc.ch>
2019-11-13 18:01:00 +0100

    add sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,5 +4,22 @@ module.exports = {
   base: '/Docs/',
   markdown: {
     lineNumbers: true
+  },
+  themeConfig: {
+    displayAllHeaders: true, // Default: false
+    nav: [{
+      text: 'Home',
+      link: '/'
+    }],
+    sidebar: [{
+      type: 'group',
+      title: '1.1',
+      collapsable: true,
+      children: [
+        ['/', '1.1.1'],
+        ['/guide/', '1.1.2']
+      ]
+    }]
+
   }
 }
